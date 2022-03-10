556531e295c6f26bc5b0bbe5d131e95dc7a30e3d
Koki Nakamura <nk.levia22@gmail.com>
2022-03-10 20:51:44 +0900

    init cookie.js

---
+++ jsutil/cookie.js
@@ -0,0 +1,12 @@
+function clearCookies() {
+  document.cookie
+    .split(";")
+    .forEach(
+      (cookie) =>
+        (document.cookie = cookie
+          .replace(/^ +/, "")
+          .replace(/=.*/, `=;expires=${new Date(0).toUTCString()};path=/`))
+    );
+}
+
+export { clearCookies };
