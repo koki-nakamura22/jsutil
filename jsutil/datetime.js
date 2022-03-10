daade6ab3de302cf4b5257052a7ce06a6338b469
Koki Nakamura <nk.levia22@gmail.com>
2022-03-10 20:51:57 +0900

    init datetime.js

---
+++ jsutil/datetime.js
@@ -0,0 +1,19 @@
+function isDateValid(...val) {
+  return !Number.isNaN(new Date(...val).valueOf());
+}
+
+function getDateOfYear(date) {
+  return Math.floor(
+    (date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24
+  );
+}
+
+function calcDayDiff(date1, date2) {
+  return Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000);
+}
+
+function timeFromDate(date) {
+  return date.toTimeString().slice(0, 8);
+}
+
+export { isDateValid, getDateOfYear, calcDayDiff, timeFromDate };
