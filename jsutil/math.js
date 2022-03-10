483f61104183f5c3859ca9c36e782b3fdde59cf9
Koki Nakamura <nk.levia22@gmail.com>
2022-03-10 21:04:59 +0900

    add an average function for array

--- jsutil/math.js
+++ jsutil/math.js
@@ -2,8 +2,16 @@ function isEven(num) {
   return num % 2 === 0;
 }
 
-function average(...numbers) {
+function __average(numbers) {
   return numbers.reduce((a, b) => a + b) / numbers.length;
 }
 
-export { isEven, average };
+function average(...numbers) {
+  return __average(numbers);
+}
+
+function averageArray(arr) {
+  return __average(arr);
+}
+
+export { isEven, average, averageArray };
