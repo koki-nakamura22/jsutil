217d0634590dcc3fcab40e5cec47bc0eba1003b0
Koki Nakamura <nk.levia22@gmail.com>
2022-03-11 18:53:11 +0900

    add escape html function

--- jsutil/string.js
+++ jsutil/string.js
@@ -2,4 +2,14 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export { capitalize };
+function escapeHtml(str) {
+  let retStr = str.replace(/&/g, "&amp;");
+  retStr = retStr.replace(/>/g, "&gt;");
+  retStr = retStr.replace(/</g, "&lt;");
+  retStr = retStr.replace(/"/g, "&quot;");
+  retStr = retStr.replace(/'/g, "&#x27;");
+  retStr = retStr.replace(/`/g, "&#x60;");
+  return retStr;
+}
+
+export { capitalize, escapeHtml };
