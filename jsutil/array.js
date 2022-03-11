38b89aa0e1f3f1ee2e38cac3b9e8b1b3c778e51b
Koki Nakamura <nk.levia22@gmail.com>
2022-03-11 18:54:11 +0900

    add a divide array function

--- jsutil/array.js
+++ jsutil/array.js
@@ -10,4 +10,15 @@ function isNotEmpty(arr) {
   return Array.isArray(arr) && arr.length > 0;
 }
 
-export { shuffleArray, removeDuplicates, isNotEmpty };
+function devideArray(baseArr, numOfDiv) {
+  const baseArrLen = baseArr.length;
+  let newArr = [];
+  for (var i = 0; i < Math.ceil(baseArrLen / numOfDiv); i++) {
+    var j = i * numOfDiv;
+    var p = baseArr.slice(j, j + numOfDiv);
+    newArr.push(p);
+  }
+  return newArr;
+}
+
+export { shuffleArray, removeDuplicates, isNotEmpty, devideArray };
