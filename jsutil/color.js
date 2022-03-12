c3a043cf33ff180272b6f6c700815d2364736b59
Koki Nakamura <nk.levia22@gmail.com>
2022-03-12 16:56:51 +0900

    add a checking a color code is valid or invalid function

--- jsutil/color.js
+++ jsutil/color.js
@@ -2,4 +2,8 @@ function generateRandomHexColor() {
   return `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
 }
 
-export { generateRandomHexColor };
+function isValidColorCode(colorCode) {
+  return /^#[0-9A-Fa-f]{6}$/.test(colorCode);
+}
+
+export { generateRandomHexColor, isValidColorCode };
