ea2f29733aab001326b94e526142568a47333ca4
Koki Nakamura <nk.levia22@gmail.com>
2022-03-12 21:04:44 +0900

    add a math test

---
+++ jsutil/__tests__/math.test.js
@@ -0,0 +1,30 @@
+"use strict";
+
+import { isEven, average, averageArray } from "../math.js";
+
+// isEven
+it("num is even", () => {
+  expect(isEven(2)).toBeTruthy();
+});
+
+it("num is not even", () => {
+  expect(isEven(1)).toBeFalsy();
+});
+
+// average
+it("average parameters (result is integer)", () => {
+  expect(average(1, 2, 3)).toEqual(2);
+});
+
+it("average parameters (result is decimal)", () => {
+  expect(average(1, 3, 4)).toEqual(2.6666666666666665);
+});
+
+// averageArray
+it("average an array (result is integer)", () => {
+  expect(averageArray([1, 2, 3])).toEqual(2);
+});
+
+it("average an array (result is decimal)", () => {
+  expect(averageArray([1, 3, 4])).toEqual(2.6666666666666665);
+});
